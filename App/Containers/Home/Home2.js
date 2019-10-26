1efd3b4e33dc24e964ce48c91115dae0cc725a39
Jonathan Gan <jonathang132298@gmail.com>
2019-10-26 19:21:28 -0400

    Not sure

---
+++ App/Containers/Home/Home2.js
@@ -0,0 +1,198 @@
+/* eslint-disable react-native/no-color-literals */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { Card } from '../../Components/Card'
+import { CardSection } from '../../Components/CardSection'
+import { WebView } from 'react-native-webview'
+
+// class Website extends React.PureComponent {
+//   render() {
+//     // const { textView } = styles
+//     return <WebView source={{ uri: 'https://amplifywebsite.herokuapp.com' }} />
+//   }
+// }
+// // const styles = {
+//   textView: {
+//     color: '#fff',
+//   },
+// }
+// import { house, Tickets, Stadium, person} from 'react-native-vector-icons/FontAwesome5'
+// import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+// import { house, Ticket, Stadium, person } from '@fortawesome/free-solid-svg-icons'
+// type Props = {}
+// const house = <FontAwesome5 name={'home'} light />
+const main = '#D8E9F0'
+class Home extends React.Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <View style={styles.container}>
+            <Text>Welcome to Amplify</Text>
+          </View>
+        </CardSection>
+      </Card>
+    )
+  }
+}
+
+class Tickets extends React.Component {
+  render() {
+    return (
+      <WebView
+        // style={{ alignItem: 'center', justifyContent: 'center', width: '80%', height: '100/%' }}
+        // style={styles.container, overflow: 'hidden'}
+        style={{
+          marginTop: '20%',
+          flex: 0.8,
+          height: 500,
+          backgroundColor: 'pink',
+          overflow: 'hidden',
+        }}
+        source={{ uri: 'https://www.youtube.com/watch?v=fwCS4Lu4qAM' }}
+        useWebKit
+        bounces={false}
+        javaScriptEnabled
+        automaticallyAdjustContentInsets={true}
+      />
+      //        <Card>
+      //   <CardSection>
+      //     <View style={styles.container}>
+      //       <Text>Tickets Screen</Text>
+      //     </View>
+      //   </CardSection>
+      // </Card>
+    )
+  }
+}
+
+class Stadiums extends React.Component {
+  render() {
+    return (
+      // <Card>
+      //   <CardSection>
+      //     <View style={styles.container}>
+      //       <Text>Stadium Screen</Text>
+      //     </View>
+      //     </CardSection>
+      // </Card>
+      <WebView
+        // style={{ alignItem: 'center', justifyContent: 'center', width: '80%', height: '100/%' }}
+        // style={styles.container, overflow: 'hidden'}
+        style={{
+          marginTop: '20%',
+          flex: 0.8,
+          height: 500,
+          backgroundColor: 'pink',
+          overflow: 'hidden',
+        }}
+        source={{ uri: 'http://campingworldstadium.io-media.com/web/index.html' }}
+        useWebKit
+        bounces={false}
+        javaScriptEnabled
+        automaticallyAdjustContentInsets={true}
+      />
+    )
+  }
+}
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <View style={styles.container}>
+            <Text>Profile Screen</Text>
+          </View>
+        </CardSection>
+      </Card>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+})
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ main }) => (
+          <View>
+            <Image source={{ uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' }} />
+            {/* <Icon style={{ color: tintColor }} size={25} name={'home'} /> */}
+            {/* <FontAwesomeIcon icon={['fab', 'house']} /> */}
+            {/* <FontAwesomeIcon icon="house" /> */}
+          </View>
+        ),
+      },
+    },
+    Tickets: {
+      screen: Tickets,
+      navigationOptions: {
+        tabBarLabel: 'Tickets',
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            {/* <Icon style={{ color: tintColor }} size={25} name={'ticket'} /> */}
+            {/* <FontAwesomeIcon icon={'ticket'} /> */}
+          </View>
+        ),
+        activeColor: '#615af6',
+        inactiveColor: '#46f6d7',
+        barStyle: { backgroundColor: '#67baf6' },
+      },
+    },
+    Stadiums: {
+      screen: Stadiums,
+      navigationOptions: {
+        tabBarLabel: 'Stadium',
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            {/* <Icon style={{ color: tintColor }} size={25} name={'stadium'} /> */}
+            {/* <FontAwesomeIcon icon="stadium" /> */}
+          </View>
+        ),
+      },
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            {/* <Icon style={{ color: tintColor }} size={25} name={'person'} /> */}
+            {/* <FontAwesomeIcon icon="person" /> */}
+          </View>
+        ),
+        activeColor: '#f60c0d',
+        inactiveColor: '#f65a22',
+        barStyle: { backgroundColor: '#f69b31' },
+      },
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    activeColor: '#f0edf6',
+    inactiveColor: '#226557',
+    barStyle: { backgroundColor: '#3BAD87' },
+  }
+)
+
+const App = createAppContainer(TabNavigator)
+
+export { App }
+
+// export { Home, Tickets, Stadium, Profile }
---
+++ ios/build/Amplify/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/RNVectorIcons.build/Objects-normal/x86_64/RNVectorIcons-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/Jons\ SDD/Code/ReactNativeApps/Amplify/ios/Pods/Target\ Support\ Files/RNVectorIcons/RNVectorIcons-dummy.m \
+  /Volumes/Jons\ SDD/Code/ReactNativeApps/Amplify/ios/Pods/Target\ Support\ Files/RNVectorIcons/RNVectorIcons-prefix.pch
